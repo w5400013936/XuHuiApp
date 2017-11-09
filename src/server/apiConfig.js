bbc622b10964a8acb06d029fbbb277a2ca5cab24
qinghuai.huang <837084459@qq.com>
2017-11-09 18:21:47 +0800

    添加网络请求代理

--- src/server/apiConfig.js
+++ src/server/apiConfig.js
@@ -5,8 +5,8 @@
 const useStaticData = false;
 const serverDebug = 'http://192.168.1.19:8000/';
 const serverRelease = 'http://192.168.1.178:9012/';
-const companyServer = 'https://bird.ioliu.cn/v1/?url=http://des.cifi.com.cn:8033/';
-// const companyServer = 'http://192.168.1.159:9029';
+//const companyServer = 'https://bird.ioliu.cn/v1/?url=http://des.cifi.com.cn:8033/';
+const companyServer = '/api';
 export default {
     companyServer,
     // 首页
