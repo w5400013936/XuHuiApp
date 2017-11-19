4df714f68c72916e5cfb16ea1fcac47ae368c62a
Administrator <837084459@qq.com>
2017-11-19 16:27:30 +0800

    admin 信息

--- src/server/globalData.js
+++ src/server/globalData.js
@@ -5,9 +5,15 @@
 export default {
     isLogin: false,
     user:{
-        guid:"582b063a5e492108dcb26339",
-        userId: 847, // 用户Id
-        name: 'houbo',  // 用户名
+        // houbo
+        // guid:"582b063a5e492108dcb26339",
+        // userId: 847, // 用户Id
+        // name: 'houbo',  // 用户名
+        // avatar: null,  //头像
+        // admin
+        guid:"5406bebbb0909c60e47752df",
+        userId: 1, // 用户Id
+        name: 'admin',  // 用户名
         avatar: null,  //头像
     },
     tabSelected:0,
